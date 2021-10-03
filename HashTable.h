e5532558fd145ce38211cd613b95a6a0bc2ba2c3
duchoang <duchoang206h@gmail.com>
2021-10-03 22:47:01 +0700

    Add class Hashtable

---
+++ HashTable.h
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+class HashTable{
+private: string Table[];
+int size ;
+public:
+void HashTable(){
+
+}
+void hashFunction(){
+
+}
+void set(key, value){
+
+}
+void get(key){
+
+}
+void remove(key){
+
+}
+void display(){
+    
+}
+}
