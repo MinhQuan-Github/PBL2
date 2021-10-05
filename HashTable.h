d67537f4043cba7f94489c1d2085a34033ea1444
duchoang <duchoang206h@gmail.com>
2021-10-05 21:39:24 +0700

    Implement Hash table

--- HashTable.h
+++ HashTable.h
@@ -2,25 +2,33 @@
 #include<string.h>
 using namespace std;
 class HashTable{
-private: string Table[];
+private:
+string Array[255];
 int size ;
 public:
-void HashTable(){
-
+HashTable(){
+	this->size = 0;
 }
-void hashFunction(){
-
+int hashFunction(string key){
+int sum = 0;
+int length = key.size();
+for(int i = 0;i<length;i++){
+    sum+=int(key.at(i));
 }
-void set(key, value){
-
+return sum % 100;
 }
-void get(key){
-
+void set(string key,string value){
+ int index = hashFunction(key);
+ this->Array[index] = value;
 }
-void remove(key){
+string get(string key){
+int index = hashFunction(key);
+return  this->Array[index];
+}
+void remove(string key){
 
 }
 void display(){
     
 }
-}
+};
