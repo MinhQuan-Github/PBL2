7e4a43cd354eb14fb9a3eaf2e4cb274c34dc5b84
duchoang <duchoang206h@gmail.com>
2021-10-25 19:56:08 +0700

    responsibilty for word.h hashtable.h

--- Word.h
+++ Word.h
@@ -5,21 +5,56 @@
 #include <windows.h>
 #include <conio.h>
 using namespace std;
-class Word{
-	private:
-		string word;
-		string type;  // adj or verb or noun 
-		string  mean;
-		string example;
-	public:
-	   string getWord(); // Lay tu ra de set index trong hash table;
-      void displayWord();
-      void displayMeaning();
-      void Input(string dong);
+class Word
+{
+private:
+	string word;
+	string type; // adj or verb or noun
+	string mean;
+	string example;
+
+public:
+	string getWord(); // Lay tu ra de set index trong hash table;
+	string getType();
+	string getMean();
+	string getExample();
+	void setWord(string m);
+	void setType(string m);
+	void setMean(string m);
+	void setExample(string m);
+	void displayWord();
+	void displayMeaning();
+	void Input(string dong);
 };
-string Word::getWord(){
-return this->word;}
-void Word::Input(string dong) {  // dong doc tu file
+string Word::getWord()
+{
+	return this->word;
+}
+string Word::getType()
+{
+	return this->type;
+}
+string Word::getMean()
+{
+	return this->mean;
+}
+string Word::getExample()
+{
+	return this->example;
+}
+void Word::setWord(string m){
+	this->word = m;
+}
+void Word::setType(string m){
+	this->type = m;
+}
+void Word::setMean(string m){
+	this->mean = m;
+}
+void Word::setExample(string m){
+	this->example = m;
+}
+/* void Word::Input(string dong) {  // dong doc tu file
 	int i, pos;
 	i = 0;
 	// about/pho tu/khoang chung;sap;gan;/He is about to die;We're about to start;How about this?;
@@ -56,13 +91,12 @@ void Word::Input(string dong) {  // dong doc tu file
 		}
 	}
 		
+}; */
+void Word::displayWord()
+{
+	cout << this->word << endl;
+	cout << this->type << endl;
+	cout << this->mean << endl;
+	cout << this->example << endl;
 };
-void Word::displayWord(){
-	cout<<this->word<<endl;
-	cout<<this->type<<endl;
-	cout<<this->mean<<endl;
-	cout<<this->example<<endl;
-	};
- #endif
-
- 
+#endif
